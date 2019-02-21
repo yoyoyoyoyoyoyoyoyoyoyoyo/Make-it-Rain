8e2b535024e14795d38e0810526a24b9cc4b41f1
Will Roske <willroske@amdg.rockhursths.edu>
2019-02-21 11:47:11 -0600

    Sprite

--- Game-of-Circles---Python-master/GameOfCircles/Enemy.py
+++ Game-of-Circles---Python-master/GameOfCircles/Enemy.py
@@ -1,23 +1,13 @@
-class Enemy:
+from Sprite import Sprite
+
+class Enemy(sprite):
     
     speed = 8
     diameter = 50
     c = color(255,200,200)
     
-    def __init__(self, x, y, team):
-        self.x = x
-        self.y = y
-        self.team = team
-        
     def move(self):
         self.x += self.speed
         if self.x < 0 or self.x > width:
             self.speed *= -1
-        
-    def display(self):
-        fill(self.c)
-        ellipse(self.x, self.y, self.diameter, self.diameter)
-        
-    def animate(self):
-        self.move()
-        self.display()
+            
---
+++ Game-of-Circles---Python-master/GameOfCircles/Sprite.py
@@ -0,0 +1,27 @@
+class Sprite:
+    team = 2
+    diameter = 50
+    c = color(255)
+    
+    def __init__(self, x, y, team):
+        self.x = x
+        self.y = y
+        self.team = team
+        
+    def move():
+        pass
+        
+    def display(self):
+        fill(self.c)
+        ellipse(self.x, self.y, self.diameter, self.diameter)
+        
+    def animate(self):
+        self.move()
+        self.display()
+        
+    def isColliding(self, other):
+        r1 = self.diameter / 2.0
+        r2 = other.diameter / 2.0
+        return r1 + r2 > dist(self.x, self.y, other.x, other.y)
+    
+    
